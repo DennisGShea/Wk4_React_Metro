e478eae6385f77d2340ca6276b33151d08073054
Dennis G Shea <DennisGSheaPhD@gmail.com>
2021-02-03 23:48:54 -0500

    metro works

---
+++ src/Metronome.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import './Metronome.css'
+
+class Metronome extends Component {
+    constructor(props) {
+        super(props)
+    this.state = {
+        playing: false, 
+        bpm: 180
+    }
+}
+    
+    handleBpmChange = ( event ) => {
+        const bpm = event.target.value;
+        this.setState({bpm})    
+    }
+
+    render() {
+        const {playing, bpm} = this.state
+        
+        return  (
+
+        <div className="metronome">
+            <div className="bpm-slider">
+            <div>{bpm} BPM </div>
+            <input type="range" min="30" max="250" value={bpm} onChange={this.handleBpmChange}/>
+        </div>
+            <button>{playing ? 'Stop' : 'Start'}</button>
+            </div>
+        )
+    }
+}
+
+export default Metronome 
